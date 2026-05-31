15929c4a5e0934bc33917e7bdb3776db800c237a
Yirenkyi123 <yirenkyicharles1@gmail.com>
2026-05-31 00:59:57 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File:initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Yirenkyi Kwasi Charles
+# STUDENT ID: 2425400788
+# Email: 2425402769@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Yirenkyi Kwasi Charles, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+# prints the initials of my name in large slylish letters
+    print()
+    print("...KYC")
+    print()
+    print("...YYY......YYY........KKK......KKK..........CCCCCCCCCC.....")
+    print("...YYY......YYY........KKK.....KKK..........CCCCCCCCCCC.....")
+    print("...YYY......YYY........KKK....KKK..........CCCC.............")
+    print("....YYY....YYY.........KKK...KKK...........CCCC.............")
+    print(".....YYY..YYY..........KKK..KKK............CCCC.............")
+    print("......YYYYYY...........KKK.KKK.............CCCC.............")
+    print(".......YYYY............KKK..KKK............CCCC.............")
+    print(".......YYYY............KKK...KKK...........CCCC.............")
+    print(".......YYYY............KKK....KKK..........CCCC.............")
+    print(".......YYYY.......**...KKK.....KKK....**....CCCCCCCCCCC...**")
+    print(".......YYYY.......**...KKK......KKK...**.....CCCCCCCCCC...**")
+    print()
 
 main()
